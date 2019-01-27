e11548c74043cf8f7c294961d63aea4f812f8274
PoldiChen <1213386222@qq.com>
2019-01-27 14:49:51 +0800

    add code files for question-java-collection.

---
+++ question-java-collection/src/main/java/question029/EnumerationTest.java
@@ -0,0 +1,21 @@
+package question029;
+
+import java.sql.CallableStatement;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+public class EnumerationTest {
+	
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		list.add("a");
+		list.add("b");
+//		Enumeration<String> enumeration = Enumeration.class;
+		CallableStatement statement;
+//		statement.call
+	}
+	
+	
+
+}
